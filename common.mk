e7e39161467c7c105cec9c1fe8f54e5a6412076a
WhiteMouseHuang <whitemouse2005@163.com>
2020-08-22 16:36:20 +0800

    添加Lawnchair

--- common.mk
+++ common.mk
@@ -1,6 +1,24 @@
+
+LOCAL_PATH := vendor/wmhuang/prebuilt
+
+## China Apps addons for exTHmUI
 # Custom packages
 PRODUCT_PACKAGES += \
     ViaBrowserCN \
     Gboard \
     Coolapk \
     GeometricWeather
+
+## Lawnchair
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    Lawnchair
+
+# Include package overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/common
+
+# Lawnchair 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LOCAL_PATH)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
