a5ff5d5637bd48043d8acbcbd683717bda06f15d
WhiteMouseHuang <whitemouse2005@163.com>
2020-08-22 20:15:53 +0800

    添加FaceUnlock

--- common.mk
+++ common.mk
@@ -23,3 +23,11 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/common
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml \
     $(LOCAL_PATH)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
+## FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := ture
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
